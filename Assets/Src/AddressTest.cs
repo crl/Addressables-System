1b46299edba695338f2dd140ec83c4ba9d64ec93
crl <crlclr@gmail.com>
2019-04-25 17:20:10 +0800

    发现不能放在Src底下，不然区分不了库

---
+++ Assets/Src/AddressTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressTest : MonoBehaviour
+{
+    public AssetReference assetReference;
+
+    private GameObject go;
+    // Start is called before the first frame update
+    async void Start()
+    {
+        go=await this.assetReference.Instantiate(Vector3.zero,Quaternion.identity).Task;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(go!=null){
+            go.transform.Rotate(new Vector3(0,60*Time.deltaTime,0),Space.Self);
+        }
+        
+    }
+}
